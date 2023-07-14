31bd2e4272d10098503b1963605e918f5d400efe
yanabilous <yanabilous05@gmail.com>
2023-07-14 13:01:04 +0300

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename='/cv_bilous/'>
           <App />
       </BrowserRouter>
   </React.StrictMode>
